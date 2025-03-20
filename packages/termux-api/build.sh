680eb9cf32a2b26a534ac71dc066c6d872fc5af1
Henrik Grimler <grimler@termux.dev>
2025-03-20 20:57:52 +0100

    termux-api: add patches to build termux-usb library as well
    Including patches to fix exec_am_broadcast.

--- packages/termux-api/build.sh
+++ packages/termux-api/build.sh
@@ -3,7 +3,12 @@ TERMUX_PKG_DESCRIPTION="Termux API commands (install also the Termux:API app)"
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=0.59.0
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/termux/termux-api-package/archive/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=c2f68cc3bd848bc386c64b06bffdb5fe96f14820017c3e80c0afc77f36a8789e
 TERMUX_PKG_BUILD_IN_SRC=true
-TERMUX_PKG_DEPENDS="bash, util-linux, termux-am (>= 0.8.0)"
+TERMUX_PKG_DEPENDS="bash, libusb, libprotobuf-c, util-linux, termux-am (>= 0.8.0)"
+
+termux_step_pre_configure() {
+	termux_setup_protobuf
+}
